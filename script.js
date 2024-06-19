a936b43c9db786dc920445bd5457803cfe95ff05
Akansha Salwan <akankshasalwan@gmail.com>
2024-06-20 00:07:31 +0530

    update

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 function toggleMobileMenu() {
     const mobileMenu = document.getElementById("mobile-menu");
-    if (mobileMenu.style.width === "250px") {
-        mobileMenu.style.width = "0";
-    } else {
+    if (mobileMenu.style.width === "0px" || mobileMenu.style.width === "") {
         mobileMenu.style.width = "250px";
+    } else {
+        mobileMenu.style.width = "0";
     }
 }
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelector('.dropbtn').addEventListener('click', function() {
+        document.querySelector('.dropdown-content').classList.toggle('show');
+    });
+
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            let dropdowns = document.getElementsByClassName("dropdown-content");
+            for (let i = 0; i < dropdowns.length; i++) {
+                let openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    };
+});
